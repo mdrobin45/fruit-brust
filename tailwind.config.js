5b1d517f717ea97a4170a6ac4f1778ccf62664b6
Md Robin Rana <developer.smrobin45@gmail.com>
2023-07-26 08:01:31 +0600

    desktop header complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
         },
         extend: {
             colors: {
-                ru: "#ff0000",
+                "main-color": "#F85559",
             },
         },
     },
