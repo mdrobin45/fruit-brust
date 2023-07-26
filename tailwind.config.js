a011039838debc63fd78db0e717b4fba7817a52b
Md Robin Rana <developer.smrobin45@gmail.com>
2023-07-26 10:29:10 +0600

    deal of the day done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./*.{html,js}"],
-    theme: {
-        container: {
-            padding: {
-                DEFAULT: "1rem",
-                md: "4rem",
-            },
-        },
-        extend: {
-            colors: {
-                "main-color": "#F85559",
-            },
-        },
-    },
-    plugins: [require("daisyui")],
+   content: ["./*.{html,js}"],
+   theme: {
+      extend: {},
+   },
+   plugins: [],
 };
